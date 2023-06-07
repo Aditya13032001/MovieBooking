0cfe3754eb405c608424a63bb205d51f80a60f30
Aditya Gaikwad <73682905+Aditya13032001@users.noreply.github.com>
2023-06-07 23:58:34 +0530

    Update Form.js

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,14 +1,30 @@
-
-
 const Form = () => {
+    const booking=()=>{
+       window.alert("Booking Done");
+    }
     return (
-        <div className="form">
-            <h1 >Book Now</h1>
-            <input type="text" className="form-control" placeholder="Name" />
-            <input type="tel" className="form-control" placeholder="Number" />
-            <button className="btn">Book Now</button>
+        <>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+            <div className="form">
 
-        </div>
+                <div class="form-group">
+                    <label for="exampleInputName1">Name address</label><br></br>
+                    <input type="Name" class="form-control" id="exampleInputName1" aria-describedby="NameHelp" placeholder="Enter Name" />
+                    <small id="NameHelp" class="form-text text-muted">We'll never share your Name with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputMobile1">Mobile</label>
+                    <input type="Mobile" class="form-control" id="exampleInputMobile1" placeholder="Mobile" />
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="submit" onClick={booking}>Book Now</button>
+
+
+            </div>
+        </>
     )
 }
 
